c2ef4a7b2b1774b6119d2c16571b0e1e7e9a01ff
yoon <1004dustkd@gmail.com>
2022-01-22 13:53:51 +0900

    [algo]BOJ_2073, BOJ_9095

---
+++ algorithm/boj/2073_수도배관공사.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int d, p;
+struct pipe {
+	int l, c;
+};
+vector<pipe> pipes;
+
+bool cmp(pipe& a, pipe& b) {
+	if (a.l == b.l)
+		return a.c < b.c;
+	return a.l < b.l;
+}
+void init() {
+	cin >> d >> p;
+	for (int i = 0; i < p; i++) {
+		int l, c;
+		cin >> l >> c;
+		pipes.push_back({ l, c });
+	}
+	sort(pipes.begin(), pipes.end(), cmp);
+}
+
+int main() {
+	fastio;
+	
+	init();
+
+	return 0;
+}
---
+++ algorithm/boj/9095_1 2 3 더하기.cpp
@@ -0,0 +1,39 @@
+﻿/**********************************************************************
+작성일 : 2022-1-22
+문제 :
+	정수 n이 주어졌을 때 1, 2, 3만을 더해서 만드는 경우의 수
+풀이 :
+	- 중복 조합 문제
+	- dfs를 활용
+시간 제한 : 1초
+메모리 제한 : 512MB
+예상 시간복잡도 : O(?)
+TEST 결과 : 통과
+**********************************************************************/
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int cnt = 0;
+
+void dfs(int n) {
+	if (n == 0) cnt++;
+
+	for (int i = 1; i <= 3; i++) {
+		if (n < i) continue;
+		dfs(n - i);
+	}
+}
+
+int main() {
+	fastio;
+
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		cnt = 0;
+		int n; cin >> n;
+		dfs(n);
+		cout << cnt << '\n';
+	}
+}
