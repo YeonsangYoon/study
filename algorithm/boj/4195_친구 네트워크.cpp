2608c82391404cd46d03142f7da978f6847ece09
yoon <1004dustkd@gmail.com>
2022-02-04 22:11:08 +0900

    [algo]BOJ_4195, BOJ_5446

---
+++ algorithm/boj/4195_친구 네트워크.cpp
@@ -0,0 +1,62 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+struct node {
+	int idx;
+	int setCnt;
+};
+node parent[200002];
+vector<pair<int, int>> network;
+map<string, int> id;
+
+int find(int a) {
+	if (parent[a].idx == a)
+		return a;
+	return find(parent[a].idx);
+}
+int merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a == b)
+		return parent[a].setCnt;
+
+	parent[a].idx = b;
+	parent[b].setCnt += parent[a].setCnt;
+	return parent[b].setCnt;
+}
+void init() {
+	vector<pair<int, int>>().swap(network);
+	map<string, int>().swap(id);
+
+	int n, cnt = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string a, b;
+		cin >> a >> b;
+		if (!id[a])
+			id[a] = cnt++;
+		if (!id[b])
+			id[b] = cnt++;
+
+		network.push_back(make_pair(id[a], id[b]));
+	}
+	for (int i = 0; i <= 200001; i++) {
+		parent[i].idx = i;
+		parent[i].setCnt = 1;
+	}
+}
+int main() {
+	fastio;
+
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		init();
+		
+		for (auto& i : network) {
+			cout << merge(i.first, i.second) << '\n';
+		}
+		continue;
+	}
+}
---
+++ algorithm/boj/5446_용량 부족.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+struct TRIE {
+	bool isData = false;
+	map<char, TRIE*> child;
+};
+
+void insert(TRIE* root, string s) {
+	for (char& c : s) {
+		if (!root->child[c])
+			root->child[c] = new TRIE();
+		root = root->child[c];
+	}
+	root->isData = true;
+}
+TRIE* build_trie(int n) {
+	TRIE* trie = new TRIE();
+
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		insert(trie, s);
+	}
+	return trie;
+}
+void debug_trie(TRIE* root, string word) {
+	map<char, TRIE*>::iterator iter;
+	for (iter = root->child.begin(); iter != root->child.end(); iter++) {
+		word.push_back(iter->first);
+		debug_trie(iter->second, word);
+		word.pop_back();
+	}
+	if (root->isData) {
+		cout << word << '\n';
+	}
+	delete root;
+}
+
+int main() {
+	fastio;
+
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		int n, m;
+		cin >> n;
+		TRIE* deleteTrie = build_trie(n);
+		
+		cin >> m;
+		if (m == 0)
+			cout << 1 << '\n';
+		TRIE* remainTrie = build_trie(m);
+	}
+}
