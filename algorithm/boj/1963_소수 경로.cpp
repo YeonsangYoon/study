d750fc3efb56ad759c6bd7833ab7e4b1f7d21a19
yoon <1004dustkd@gmail.com>
2022-03-12 21:31:29 +0900

    [algo]BOJ_1963, BOJ_9466

---
+++ algorithm/boj/1963_소수 경로.cpp
@@ -0,0 +1,93 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, m;
+int visited[10000];
+bool isPrime[10000];
+vector<int> adjlist[10000];
+
+void get_string(int num, int arr[]) {
+	for (int i = 3; i >= 0; i--) {
+		arr[i] = (num % 10);
+		num /= 10;
+	}
+}
+int get_number(int arr[]) {
+	int ret = 0;
+	for (int i = 0; i < 4; i++) {
+		ret = ret * 10 + arr[i];
+	}
+	return ret;
+}
+
+void init() {
+	memset(isPrime, true, sizeof(isPrime));
+	for (int i = 2; i < 10000; i++) {
+		if (isPrime[i]) {
+			for (int j = i * 2; j < 10000; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	for (int i = 1000; i < 10000; i++) {
+		if (!isPrime[i]) continue;
+
+		int arr[4];
+		get_string(i, arr);
+		
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 10; k++) {
+				if ((j == 0 && k == 0) || arr[j] == k)
+					continue;
+
+				int tmp = arr[j];
+				arr[j] = k;
+				int num = get_number(arr);
+				if(isPrime[num])
+					adjlist[i].push_back(num);
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+
+int bfs(int s, int e) {
+	if (s == e) return 0;
+
+	memset(visited, 0, sizeof(visited));
+	queue<int> q;
+	q.push(s);
+
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+
+		for (int& next : adjlist[now]) {
+			if (visited[next] > 0) continue;
+
+			visited[next] = visited[now] + 1;
+			q.push(next);
+		}
+	}
+	
+	return visited[e] ? visited[e] : -1;
+}
+
+int main() {
+	fastio;
+
+	init();
+
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		int s, e;
+		cin >> s >> e;
+		
+		int result = bfs(s, e);
+		if (result >= 0)
+			cout << result << '\n';
+		else
+			cout << "Impossible\n";
+	}
+}
---
+++ algorithm/boj/9466_텀 프로젝트.cpp
@@ -0,0 +1,76 @@
+﻿/**********************************************************************
+작성일 : 2022-3-12
+문제 :
+	1~n까지의 학생이 각자 프로젝트를 하고 싶은 팀원이 있을 때
+	서로 원하는 팀원이 맞을 때 프로젝트 팀이 만들어진다. 이때 어느 팀에도
+	속하지 않는 학생의 수를 구하라.
+풀이 :
+	유향 그래프에서 사이클을 찾는 알고리즘
+	보통 dfs로 구현하지만, 위상정렬도 가능하기 때문에 위상정렬로 구했다.
+	위상정렬 수행후, indegree가 0보다 큰 경우 사이클이다.
+시간 제한 : 3초
+메모리 제한 : 256MB
+예상 시간복잡도 : O(n)
+TEST 결과 : 통과
+**********************************************************************/
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_SIZE 100010
+using namespace std;
+
+int n;
+int inDegree[MAX_SIZE];
+vector<int> adjlist[MAX_SIZE];
+
+
+void init() {
+	cin >> n;
+
+	memset(inDegree, 0, sizeof(inDegree));
+	for (int i = 1; i <= n; i++) {
+		adjlist[i].clear();
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int id; cin >> id;
+		adjlist[i].push_back(id);
+		inDegree[id]++;
+	}
+}
+void topological_sort() {
+	// 위상정렬 
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		if (inDegree[i] == 0)
+			q.push(i);
+	}
+
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+
+		for (int& next : adjlist[now]) {
+			if (--inDegree[next] == 0) {
+				q.push(next);
+			}
+		}
+	}
+}
+
+
+int main() {
+	fastio;
+
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		init();		// input 및 변수 초기화
+		topological_sort();	// 위상정렬 수행
+
+		// indegree가 0이상이면 사이클
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (inDegree[i] > 0)
+				cnt++;
+		}
+		cout << n - cnt << '\n';
+	}
+}
