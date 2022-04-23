28194f09fb28a0f47b495f977cad790819a80fbc
yoon <1004dustkd@gmail.com>
2022-04-23 23:36:59 +0900

    [algo]BOJ_16234

---
+++ algorithm/boj/16234_인구 이동.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, l, r;
+int world[51][51];
+int check[51][51];
+
+bool move() {
+	
+}
+
+void init() {
+	cin >> n >> l >> r;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> world[i][j];
+		}
+	}
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	int day = 0;
+	while (1) {
+
+	}
+}
