506e8d85a5f047aa0c5ab5ed2cbd48ed8c1981e4
yoon <1004dustkd@gmail.com>
2022-05-14 23:03:48 +0900

    [algo]BOJ_5021

---
+++ algorithm/boj/5021_왕위 계승.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, m;
+string king;
+vector<int> adjlist[1000];
+map<string, int> id;
+string name[1000];
+
+void init() {
+	cin >> n >> m;
+	cin >> king;
+
+	int ptr = 1;
+	id[king] = ptr;
+	name[ptr] = king;
+
+	for (int i = 0; i < n; i++) {
+		string c, f, m;
+		cin >> c >> f >> m;
+		if (!id[c]) {
+			id[c] = ptr;
+			name[ptr++] = c;
+		}
+		if (!id[c]) {
+			id[c] = ptr;
+			name[ptr++] = c;
+		}
+	}
+}
+
+int main(void) {
+	fastio;
+	init();
+}
