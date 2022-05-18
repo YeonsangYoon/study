218c86aabee46776fb49ecb97c1a21f67d0ddfcd
yoon <1004dustkd@gmail.com>
2022-05-18 22:56:44 +0900

    [algo]BOJ_1253

---
+++ algorithm/boj/1253_좋다.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+int number[2001];
+
+void init() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> number[i];
+	}
+	sort(number + 1, number + n);
+}
+
+int main(void) {
+	fastio;
+	init();
+
+}
