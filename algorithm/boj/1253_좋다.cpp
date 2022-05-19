97a6ccdd46800c051dcb0b602502b02f4fa65fc5
yoon <1004dustkd@gmail.com>
2022-05-19 22:27:22 +0900

    [algo]BOJ_1253(modify)

--- algorithm/boj/1253_좋다.cpp
+++ algorithm/boj/1253_좋다.cpp
@@ -4,17 +4,54 @@ using namespace std;
 
 int n;
 int number[2001];
+bool isGoodNumber[2001];
+
+int get_idx(int num) {
+	int l = 1, r = n, mid;
+	while (l < r) {
+		mid = (l + r) / 2;
+
+		if (number[mid] >= num)
+			r = mid;
+		else
+			l = mid + 1;
+	}
+	return r;
+}
+
+int get_gn_count() {
+	int sum, idx;
+	for (int i = 1; i <= n; i++) {
+		for (int j = i + 1; j <= n; j++) {
+			sum = number[i] + number[j];
+			idx = get_idx(sum);
+			
+			for (int k = idx; k <= n; k++) {
+				if (number[k] != sum) break;
+				else if (k == i || k == j) continue;
+
+				isGoodNumber[k] = true;
+			}
+		}
+	}
+	int cnt = 0;
+	for (int i = 1; i <= n; i++)
+		if (isGoodNumber[i])
+			cnt++;
+	return cnt;
+}
 
 void init() {
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> number[i];
 	}
-	sort(number + 1, number + n);
+	sort(number + 1, number + n + 1);
 }
 
 int main(void) {
 	fastio;
 	init();
 
+	cout << get_gn_count();
 }
