b5135ee7d081c001440aeb18f40793e689537901
yoon <1004dustkd@gmail.com>
2022-05-23 22:52:11 +0900

    [algo]BOJ_12869

---
+++ algorithm/boj/12869_뮤탈리스크.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+int hp[3];
+int damage[3] = { 9, 3, 1 };
+
+void init() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> hp[i];
+}
+
+int main(void) {
+	fastio;
+	init();
+}
