4b27f70177325774c3a0da9a34141896ebe637c9
yoon <1004dustkd@gmail.com>
2022-06-25 23:36:04 +0900

    [algo]BOJ_13317

---
+++ algorithm/boj/13317_한번남았다.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main(void) {
+	fastio;
+
+	int n = 50;
+	int m = 50;
+
+	cout << n << ' ' << m << '\n';
+	cout << "1 50 -1\n";
+	for (int i = 2; i < m; i++) {
+		cout << i << ' ' << (i + n - 1) % n << ' ' << -1 << '\n';
+	}
+	cout << "50 49 1";
+}
