ba7f5995652165d8c49be3b49fa0132a947f893c
yoon <1004dustkd@gmail.com>
2022-06-29 21:01:10 +0900

    [algo]BOJ_1750

---
+++ algorithm/boj/1750_서로소의 개수.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+int s[50];
+
+int gcd(int a, int b) {
+	int r;
+	while (b > 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+void init() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cout << s[i];
+}
+
+int main(void) {
+	fastio;
+
+
+}
