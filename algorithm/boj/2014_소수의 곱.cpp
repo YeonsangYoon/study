bfd5b939754dc59073b3ed684aad2d3a5278f1dc
yoon <1004dustkd@gmail.com>
2022-07-02 22:00:09 +0900

    [algo]BOJ_2014(finished)

--- algorithm/boj/2014_소수의 곱.cpp
+++ algorithm/boj/2014_소수의 곱.cpp
@@ -1,38 +1,38 @@
-﻿/*
-*  k^0 + k^1 + k^2 + k^3
-*/
+﻿/**********************************************************************
+작성일 : 2022-7-02
+문제 :
+	k개의 소수가 있다. 이 소수들 중에서 몇개를 곱해서 얻게되는 수들이 있을때,
+	n번째의 수를 구하라.
+풀이 :
+	우선순위 큐를 활용하여 매 수를 뽑을 때 가장 작은 수를 선택하고
+	해당 수에 k개의 수를 곱해서 다시 큐에 push한다.
+	이때, 중복을 제거하기 위한 조건문을 추가하였다.
+시간 제한 : 2초
+메모리 제한 : 128MB
+예상 시간복잡도 :
+TEST 결과 : 통과
+**********************************************************************/
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
 typedef long long ll;
 
-int n, k, len, maxVal;
+int n, k;
 vector<ll> primes;
-map<ll, bool> isIn;
+priority_queue<ll, vector<ll>, greater<ll>> pq;
 
 int solve() {
-	priority_queue<ll, vector<ll>, greater<ll>> pq;
-	for (ll& p : primes) {
-		pq.push(p);
-		isIn[p] = true;
-	}
-
-	int cnt = 0;
-	ll now = 0;
-	while (cnt < n) {
-			now = pq.top(); pq.pop();
+	for (int i = 0; i < n - 1;i++) {
+		ll now = pq.top(); pq.pop();
 
 		for (ll& p : primes) {
-			ll next = p * now;
-			if (!isIn[next] && (int)pq.size() < n) {
-				pq.push(next);
-				isIn[next] = true;
-			}
+			pq.push(now * p);
+			if (now % p == 0)
+				break;
 		}
-		cnt++;
 	}
-	return now;
+	return pq.top();
 }
 
 void init() {
@@ -40,6 +40,7 @@ void init() {
 	for (int i = 0; i < k; i++) {
 		int prime; cin >> prime;
 		primes.push_back(prime);
+		pq.push(prime);
 	}
 }
 
