bc677c4213d52827aa4922667c1c8c3eda9fff0e
yoon <1004dustkd@gmail.com>
2022-07-11 23:08:52 +0900

    [algo]BOJ_9328(modify)

--- algorithm/boj/9328_열쇠.cpp
+++ algorithm/boj/9328_열쇠.cpp
@@ -1,9 +1,23 @@
-﻿#include <bits/stdc++.h>
+﻿/*
+* 1. 현재 들고있는 열쇠로 문 제거
+* 2. 열쇠 줍기
+* 3. 주운 열쇠가 없으면 종료
+* 4. 이제 훔친다.
+*/
+#include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
+struct pos {
+	int r, c;
+};
+
 int h, w;
+bool visited[101][101];
 char building[101][101];
+queue<char> key;
+
+
 
 void init_testcase() {
 	cin >> h >> w;
@@ -12,6 +26,9 @@ void init_testcase() {
 			cin >> building[i][j];
 		}
 	}
+	string s; cin >> s;
+	for (char& c : s)
+		key.push(c);
 }
 
 int main(void) {
