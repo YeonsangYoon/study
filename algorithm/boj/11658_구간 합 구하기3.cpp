529c9907a09f19f340c917cfb1fab9a8c971cea9
yoon <1004dustkd@gmail.com>
2022-07-13 22:37:54 +0900

    [algo]BOJ_11658

---
+++ algorithm/boj/11658_구간 합 구하기3.cpp
@@ -0,0 +1,12 @@
+﻿/*
+* 구간 합 구하기3 문제 분석
+* N x N의 표가 있을 때, 해당 표의 구간합을 구하는 프로그램을 구해야함.
+*/
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main(void) {
+	fastio;
+	
+}
