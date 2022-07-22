c025c9cd0d7ca59c8de131b3fc49c07b73b8d3f0
yoon <1004dustkd@gmail.com>
2022-07-22 23:37:31 +0900

    [algo]BOJ_1629

---
+++ algorithm/boj/1629_곱셈.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+typedef long long ll;
+
+ll a, b, c;
+
+ll solve() {
+	ll ret = 1;
+	ll cur = a;
+
+	for (int i = 0; i < 32; i++) {
+		if (((ll)1 << i) & b) {
+			ret *= cur;
+			ret %= c;
+		}
+		cur = (cur * cur) % c;
+	}
+
+	return ret;
+}
+
+int main(void) {
+	fastio;
+	cin >> a >> b >> c;
+
+	cout << solve();
+}
