e3546a3173c0fe4c3e1b8264843b2670cb343b63
yoon <1004dustkd@gmail.com>
2022-07-25 23:13:55 +0900

    [algo]BOJ_12853

---
+++ algorithm/boj/12853_1로 만들기 2.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+#define MAX_N 1000001
+
+int n;
+int dp[MAX_N], parent[MAX_N];
+
+void init() {
+	cin >> n;
+}
+
+int main(void) {
+	fastio;
+
+}
