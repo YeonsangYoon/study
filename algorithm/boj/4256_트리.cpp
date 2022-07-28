4f4d69db40f664eed76d56f8a5725c2d8a4151f3
yoon <1004dustkd@gmail.com>
2022-07-28 23:35:53 +0900

    [algo]BOJ_4256

---
+++ algorithm/boj/4256_트리.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cout.tie(0), cin.tie(0)
+using namespace std;
+
+int n;
+int pre[1001], in[1001];
+
+void init() {
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> pre[i];
+	for (int i = 1; i <= n; i++)
+		cin >> in[i];
+}
+
+int main(void) {
+	fastio;
+
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		init();
+
+	}
+}
