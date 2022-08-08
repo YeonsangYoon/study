286c6acb104a7d652a619cf4f2805d145fc7588b
yoon <1004dustkd@gmail.com>
2022-08-08 23:24:05 +0900

    [algo]BOJ_9372

---
+++ algorithm/boj/9372_상근이의 여행.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+struct disJointSet {
+	int parent[1001];
+	void init(int a) {
+		for (int i = 1; i <= a; i++)
+			parent[i] = i;
+	}
+	int find(int a) {
+		if (a == parent[a]) return a;
+		return parent[a] = find(parent[a]);
+	}
+	void merge(int a, int b) {
+		a = find(a); b = find(b);
+		if (a == b) return;
+		parent[a] = b;
+	}
+};
+
+int n, m;
+disJointSet djs;
+
+void init() {
+	cin >> n >> m;
+	djs.init(n);
+}
+
+int main(void) {
+	fastio;
+
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		init();
+
+	}
+}
