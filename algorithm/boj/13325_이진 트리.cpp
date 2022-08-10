e612b15a32e45d4cda7a5497520ad6fb8568186d
yoon <1004dustkd@gmail.com>
2022-08-10 23:28:57 +0900

    [algo]BOJ_13325

---
+++ algorithm/boj/13325_이진 트리.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int k;
+
+void init() {
+	cin >> k;
+}
+
+int main(void) {
+	fastio;
+
+}
