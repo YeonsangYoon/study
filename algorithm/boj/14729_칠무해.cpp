c06397c6cd6eacdb15d39391f981d2c5eb5fe3e6
yoon <1004dustkd@gmail.com>
2022-08-21 23:39:23 +0900

    [algo]BOJ_14729

---
+++ algorithm/boj/14729_칠무해.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+priority_queue<double, vector<double>, greater<double>> score;
+
+void init() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		double tmp;
+		cin >> tmp;
+		score.push(tmp);
+	}
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	cout << fixed;
+	cout.precision(3);
+
+	for (int i = 0; i < 7; i++) {
+		cout << score.top() << '\n';
+		score.pop();
+	}
+}
