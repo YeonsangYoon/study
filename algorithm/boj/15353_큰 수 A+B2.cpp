ae92118d90e86c0a4081bdfd219474fa091ee14f
yoon <1004dustkd@gmail.com>
2022-08-28 23:37:58 +0900

    [algo]BOJ_15353

---
+++ algorithm/boj/15353_큰 수 A+B2.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+int arr[10001], brr[10001];
+string a, b;
+
+string solve() {
+	string ret;
+
+	int prev = 0;
+	for (int i = 0; i < n; i++) {
+		int sum = arr[i] + brr[i] + prev;
+		ret.push_back(sum % 10 + '0');
+		prev = sum / 10;
+	}
+	if(prev > 0)
+		ret.push_back(prev + '0');
+	reverse(ret.begin(), ret.end());
+	return ret;
+}
+
+void init() {
+	cin >> a >> b;
+	int alen = (int)a.size();
+	int blen = (int)b.size();
+	n = max(alen, blen);
+
+	for (int i = 0; i < alen; i++)
+		arr[i] = a[alen - 1 - i] - '0';
+	for (int i = 0; i < blen; i++)
+		brr[i] = b[blen - 1 - i] - '0';
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	cout << solve();
+}
