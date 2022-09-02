32e864bb9bf46015834e5c19efed623a9d3eab77
yoon <1004dustkd@gmail.com>
2022-09-02 22:21:21 +0900

    [algo]BOJ_15650

---
+++ algorithm/boj/15650_N과 M2.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, m;
+vector<int> cur;
+
+void dfs(int s, int cnt) {
+	if (cnt == m) {
+		for (int& i : cur)
+			cout << i << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = s; i <= n; i++) {
+		cur.push_back(i);
+		dfs(i + 1, cnt + 1);
+		cur.pop_back();
+	}
+}
+
+int main(void) {
+	fastio;
+	cin >> n >> m;
+
+	dfs(1, 0);
+}
