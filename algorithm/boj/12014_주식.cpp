080f71cfbeee99a9697e060b12bb1e6b1c54c4a4
yoon <1004dustkd@gmail.com>
2022-09-07 22:17:22 +0900

    [algo]BOJ_12014(finish)

--- algorithm/boj/12014_주식.cpp
+++ algorithm/boj/12014_주식.cpp
@@ -2,11 +2,54 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-void init(){
+int n, k;
+int stock[10001];
+vector<int> index_list;
 
+int get_index(int num) {
+	int l = 0, r = (int)index_list.size();
+
+	while (l < r) {
+		int mid = (l + r) / 2;
+
+		if (index_list[mid] >= num)
+			r = mid;
+		else
+			l = mid + 1;
+	}
+	return l;
+}
+
+int LIS() {
+	for (int i = 0; i < n; i++) {
+		int idx = get_index(stock[i]);
+
+		if (idx < (int)index_list.size()) {
+			index_list[idx] = stock[i];
+		}
+		else {
+			index_list.push_back(stock[i]);
+		}
+	}
+	return (int)index_list.size();
+}
+
+void init(){
+	index_list.clear();
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> stock[i];
+	}
 }
 
 int main(void) {
 	fastio;
-	init();
+	
+	int tc; cin >> tc;
+	for (int i = 1; i <= tc; i++) {
+		init();
+		
+		cout << "Case #" << i << '\n';
+		cout << (k <= LIS()) << '\n';
+	}
 }
