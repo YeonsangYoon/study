af0b2cc4edb9e773937679023c920e0b79c545f1
yoon <1004dustkd@gmail.com>
2022-09-12 22:28:58 +0900

    [algo]BOJ_16474

---
+++ algorithm/boj/16474_이상한 전깃줄.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, m;
+
+void init() {
+	cin >> n >> m;
+
+}
+
+int main(void) {
+	fastio;
+	init();
+
+}
