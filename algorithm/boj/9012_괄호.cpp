050a67767fdea63a0bf824f72f857038d0f93859
yoon <1004dustkd@gmail.com>
2022-09-15 21:04:49 +0900

    [algo]BOJ_9012

---
+++ algorithm/boj/9012_괄호.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+string s;
+
+bool isValid() {
+	stack<char> st;
+
+	for (char& c : s) {
+		if (st.empty() || c == '(')
+			st.push(c);
+		else if (st.top() == '(')
+			st.pop();
+		else
+			st.push(c);
+	}
+	return st.empty();
+}
+
+void init() {
+	s.clear();
+	cin >> s;
+}
+
+int main(void) {
+	fastio;
+	
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		init();
+
+		if (isValid())
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+}
