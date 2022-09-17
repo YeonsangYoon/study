9941a4f9b0123ff72880eeb30b87377ef4126da7
yoon <1004dustkd@gmail.com>
2022-09-17 23:19:36 +0900

    [algo]BOJ_11931

---
+++ algorithm/boj/11931_수 정렬하기4.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+vector<int> arr;
+
+void init() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		arr.push_back(-num);
+	}
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	sort(arr.begin(), arr.end());
+
+	for (int& i : arr)
+		cout << -i << '\n';
+}
