be5264021822566c2b126e98a0f736371f6da5c2
yoon <1004dustkd@gmail.com>
2022-09-20 23:07:46 +0900

    [algo]BOJ_13711

---
+++ algorithm/boj/13711_LCS4.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+vector<int> arr1, arr2;
+
+void init() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		arr1.push_back(num);
+	}
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		arr2.push_back(num);
+	}
+}
+
+int main(void){
+	fastio;
+	init();
+}
