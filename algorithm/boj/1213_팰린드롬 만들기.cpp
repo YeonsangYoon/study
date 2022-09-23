2eeaa65b2849f917b3bee4cb2a9d191c1fffecc8
yoon <1004dustkd@gmail.com>
2022-09-23 22:56:49 +0900

    [algo]BOJ_1213

---
+++ algorithm/boj/1213_팰린드롬 만들기.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cout.tie(0), cin.tie(0)
+using namespace std;
+
+
+int chcnt[26];
+
+int init() {
+	string s;
+	cin >> s;
+	
+	for (char& c : s)
+		chcnt[c - 'A']++;
+
+	int idx = 26, cnt = 0;
+	for (int i = 0; i < 26; i++) {
+		if (chcnt[i] % 2) {
+			cnt++;
+			idx = i;
+		}
+	}
+	return (cnt > 1) ? -1 : idx;
+}
+
+int main(void) {
+	fastio;
+	int center = init();
+
+	if (center == -1) {
+		cout << "I'm Sorry Hansoo";
+		return 0;
+	}
+
+	string s;
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < chcnt[i] / 2; j++)
+			s.push_back('A' + i);
+	}
+
+	cout << s;
+	if (center < 26) {
+		char c = center + 'A';
+		cout << c;
+	}
+	reverse(s.begin(), s.end());
+	cout << s;
+}
