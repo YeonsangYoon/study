2389deccea6cf9d69fba67e1adf68ed999c8a044
yoon <1004dustkd@gmail.com>
2022-09-24 23:23:49 +0900

    [algo]BOJ_1577

---
+++ algorithm/boj/1577_도로의 개수.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, m, k;
+
+void init() {
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < k; i++) {
+
+	}
+}
+
+int main(void) {
+	fastio;
+
+}
