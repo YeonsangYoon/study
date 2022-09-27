e141cf49f2eef51923bacda596795c03f5eb8f84
yoon <1004dustkd@gmail.com>
2022-09-27 22:21:52 +0900

    [algo]BOJ_2839

---
+++ algorithm/boj/2839_설탕 배달.cpp
@@ -0,0 +1,22 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main(void) {
+	fastio;
+	int n;
+	cin >> n;
+
+	int cnt = 0;
+	while (n > 0) {
+		if (n % 5 == 0)
+			break;
+		n -= 3;
+		cnt++;
+	}
+
+	if (n < 0)
+		cout << -1;
+	else
+		cout << cnt + n / 5;
+}
