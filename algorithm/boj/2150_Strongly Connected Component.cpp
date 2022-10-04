87f2a7d7f4f16f7e3c1e560960cf9c397e3ffc8b
yoon <1004dustkd@gmail.com>
2022-10-04 22:28:21 +0900

    [algo]BOJ_2150(finished)

--- algorithm/boj/2150_Strongly Connected Component.cpp
+++ algorithm/boj/2150_Strongly Connected Component.cpp
@@ -2,63 +2,55 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-int n, m;
-int id[10001];
-bool visited[10001];
-stack<int> st;
-vector<int> adjlist[10001], radjlist[10001];
-vector<vector<int>> scc;
+#define N_SIZE 10000
 
-void dfs(int cur) {
+int n, m;
+int sccID[N_SIZE + 1];
+bool visited[N_SIZE + 1];
+stack<int> st;
+vector<int> adjlist[N_SIZE + 1], radjlist[N_SIZE + 1];
+vector<vector<int>> sccList;
+
+void dfs1(int cur) {
 	visited[cur] = true;
 
 	for (int& nex : adjlist[cur]) {
 		if (!visited[nex])
-			dfs(nex);
+			dfs1(nex);
 	}
 	st.push(cur);
 }
-
-void add_node(int cur, int sccid) {
+void dfs2(int cur, int id, vector<int> &v) {
 	visited[cur] = true;
-	id[cur] = sccid;
+	sccID[cur] = id;
+	v.push_back(cur);
 
 	for (int& nex : radjlist[cur]) {
 		if (!visited[nex])
-			add_node(nex, sccid);
+			dfs2(nex, id, v);
 	}
 }
 
-void print_scc() {
+int get_scc() {
 	for (int i = 1; i <= n; i++) {
 		if (!visited[i])
-			dfs(i);
+			dfs1(i);
 	}
 	memset(visited, false, sizeof(visited));
 
-	int sccid = 1;
+	int id = 0;
 	while (!st.empty()) {
 		int now = st.top(); st.pop();
+
 		if (!visited[now]) {
-			add_node(now, sccid);
-			sccid++;
+			vector<int> tmp;
+			dfs2(now, id, tmp);
+			sort(tmp.begin(), tmp.end());
+			sccList.push_back(tmp);
+			id++;
 		}
 	}
-	memset(visited, false, sizeof(visited));
-	for (int i = 1; i <= n; i++) {
-		if (!visited[i]) {
-			int sccid = id[i];
-			visited[i] = true;
-			cout << i << ' ';
-			for (int j = i + 1; j <= n; j++) {
-				if (id[j] == sccid) {
-					cout << j << ' ';
-					visited[j] = true;
-				}
-			}
-			cout << -1 << '\n';
-		}
-	}
+	return id;
 }
 
 void init() {
@@ -74,6 +66,17 @@ void init() {
 int main(void) {
 	fastio;
 	init();
-
-	print_scc();
+	
+	int cnt = get_scc();
+	memset(visited, false, sizeof(visited));
+	cout << cnt << '\n';
+	for (int i = 1; i <= n; i++) {
+		if (!visited[i]) {
+			for (int& j : sccList[sccID[i]]) {
+				cout << j << ' ';
+				visited[j] = true;
+			}
+			cout << -1 << '\n';
+		}
+	}
 }
