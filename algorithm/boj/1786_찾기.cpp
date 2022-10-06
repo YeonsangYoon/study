4e67083d0b9f8ad1c3038e1b5527fea27871d858
yoon <1004dustkd@gmail.com>
2022-10-06 22:30:09 +0900

    [algo]1786_찾기(finished)

--- algorithm/boj/1786_찾기.cpp
+++ algorithm/boj/1786_찾기.cpp
@@ -7,23 +7,50 @@ void init(string& a, string& b) {
 	getline(cin, b);
 }
 
-vector<int> get_table(string& s) {
-	int m = (int)s.size();
+vector<int> get_table(string& str) {
+	int m = (int)str.size();
 	vector<int> table(m, 0);
 
-	for (int begin = 1; begin < m; begin++) {
-		for (int i = 0; begin + i < m; i++) {
-			if (s[begin + i] != s[i])
-				break;
-			table[begin + i] = max(table[begin + i], i + 1);
+	int s = 1, len = 0;
+	while (s + len < m) {
+		if (str[len] == str[s + len]) {
+			len++;
+			table[s + len - 1] = len;
+		}
+		else {
+			if (len == 0)
+				s++;
+			else {
+				s += len - table[len - 1];
+				len = table[len - 1];
+			}
 		}
 	}
 	return table;
 }
 
 vector<int> find_string(string& src, string& search) {
+	int n = (int)src.size(), m = (int)search.size();
+	vector<int> ret;
 	vector<int> table = get_table(search);
+	int begin = 0, matched = 0;
 
+	while (begin <= n - m) {
+		if (matched < m && search[matched] == src[begin + matched]) {
+			matched++;
+			if (matched == m)
+				ret.push_back(begin + 1);
+		}
+		else {
+			if (matched == 0)
+				begin++;
+			else {
+				begin += matched - table[matched - 1];
+				matched = table[matched - 1];
+			}
+		}
+	}
+	return ret;
 }
 
 int main(void) {
@@ -32,5 +59,8 @@ int main(void) {
 	string a, b;
 	init(a, b);
 
-
+	vector<int> result = find_string(a, b);
+	cout << (int)result.size() << '\n';
+	for (int& i : result)
+		cout << i << ' ';
 }
