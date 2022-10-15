137c442d4153cd682481fc8e3e811fa4691e74f6
yoon <1004dustkd@gmail.com>
2022-10-15 23:43:37 +0900

    [algo]BOJ_13246(modify)

--- algorithm/boj/13246_행렬 제곱의 합.cpp
+++ algorithm/boj/13246_행렬 제곱의 합.cpp
@@ -4,47 +4,80 @@ using namespace std;
 
 #define MOD 1000
 typedef long long ll;
-typedef vector<vector<int>> matrix;
+
+struct matrix {
+	vector<vector<int>> M;
+	matrix(int n = 0) : M(n, vector<int>(n)) {}
+	vector<int>& operator [] (int i) { return M[i]; }
+	matrix& operator += (matrix m) { return *this = *this + m; }
+	matrix& operator *= (matrix m) { return *this = *this * m; }
+	friend matrix operator + (matrix a, matrix b) {
+		int n = a.M.size(); 
+		matrix ret(n);
+		for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) 
+			ret[i][j] = (a[i][j] + b[i][j]) % 1000;
+		return ret;
+	}
+	friend matrix operator * (matrix a, matrix b) {
+		int n = a.M.size(); 
+		matrix ret(n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					ret[i][j] += a[i][k] * b[k][j];
+					ret[i][j] %= 1000;
+				}
+			}
+		}
+		return ret;
+	}
+};
 
 int n, Pmax = 39;
 ll p;
-matrix dp[40];
+matrix A, E;
 
-matrix mul(matrix& a, matrix& b) {
-	matrix ret;
+void print_matrix(matrix& m) {
 	for (int i = 0; i < n; i++) {
-		vector<int> v;
 		for (int j = 0; j < n; j++) {
-			int tmp = 0;
-			for (int k = 0; k < n; k++) {
-				tmp += (a[i][k] * b[k][j]);
-				tmp %= MOD;
-			}
-			v.push_back(tmp);
+			cout << m[i][j] << ' ';
 		}
-		ret.push_back(v);
+		cout << '\n';
+	}
+}
+
+matrix _pow(matrix& m, ll b) {
+	matrix ret = E;
+	while (b) {
+		if (b & 1)
+			ret *= m;
+		m *= m;
+		b >>= 1;
 	}
 	return ret;
 }
 
 void init() {
 	cin >> n >> p;
-
+	matrix a(n), e(n);
 	for (int i = 0; i < n; i++) {
-		vector<int> v;
 		for (int j = 0; j < n; j++) {
-			int tmp; cin >> tmp;
-			v.push_back(tmp);
+			cin >> a[i][j];
+			e[i][j] = (i == j) ? 1 : 0;
 		}
-		dp[0].push_back(v);
 	}
+	A = a; E = e;
+}
 
-	for (int i = 1; i <= Pmax; i++) {
-		dp[i] = mul(dp[0], dp[0]);
-	}
+matrix solve(matrix m, ll b) {
+	if (b == 1)
+		return E;
+
+	
 }
 
 int main(void) {
 	fastio;
 	init();
+
 }
