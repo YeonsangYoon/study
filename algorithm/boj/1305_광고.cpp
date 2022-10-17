3c42f8b99a3851dc2fbdf354090300d559575f64
yoon <1004dustkd@gmail.com>
2022-10-17 18:29:37 +0900

    [algo]BOJ_1305

---
+++ algorithm/boj/1305_광고.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int l;
+vector<int> dp;
+string ad;
+
+int solve() {
+	int s = 1, len = 0;
+	while (s + len < l) {
+		if (ad[len] == ad[s + len]) {
+			len++;
+			dp[s + len - 1] = len;
+		}
+		else {
+			if (len == 0)
+				s++;
+			else {
+				s += len - dp[len - 1];
+				len = dp[len - 1];
+			}
+		}
+	}
+
+	return l - dp[l - 1];
+}
+
+void init() {
+	cin >> l >> ad;
+	dp = vector<int>(l, 0);
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	cout << solve();
+}
