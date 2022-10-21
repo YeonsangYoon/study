7d7fca5143f04d45baec7d76a7e885b93d303a78
yoon <1004dustkd@gmail.com>
2022-10-21 22:06:34 +0900

    [algo]BOJ_2193

---
+++ algorithm/boj/2193_이친수.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+long long dp[91][2];
+
+long long solve() {
+	dp[1][1] = 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+		dp[i][1] = dp[i - 1][0];
+	}
+	return dp[n][0] + dp[n][1];
+}
+
+void init() {
+	cin >> n;
+}
+
+int main(void) {
+	fastio;
+	init();
+
+	cout << solve();
+}
