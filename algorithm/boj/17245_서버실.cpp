614ea5b1127e1280585e5ba78abcb7246afa88a7
yoon <1004dustkd@gmail.com>
2022-10-22 23:11:07 +0900

    [algo]BOJ_17245

---
+++ algorithm/boj/17245_서버실.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n, totalCnt;
+vector<vector<int>> server;
+
+
+
+void init() {
+	cin >> n;
+	server = vector<vector<int>>(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> server[i][j];
+			totalCnt += server[i][j];
+		}
+	}
+}
+
+int main(void) {
+	fastio;
+
+}
