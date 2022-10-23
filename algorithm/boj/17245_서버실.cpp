5c5da7203cbed5c05261ca702c8aab7d1323c50e
yoon <1004dustkd@gmail.com>
2022-10-23 22:30:33 +0900

    [algo]BOJ_17245(finished)

--- algorithm/boj/17245_서버실.cpp
+++ algorithm/boj/17245_서버실.cpp
@@ -2,24 +2,42 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-int n, totalCnt;
-vector<vector<int>> server;
+#define int int64_t
 
+int n, totalCnt, maxFloor;
+vector<int> server;
 
+int solve() {
+	int l = 0, r = maxFloor;
+
+	while (l < r) {
+		int mid = (l + r) / 2;
+		int cnt = 0;
+		for (int& com : server)
+			cnt += (com > mid) ? mid : com;
+
+		if (2 * cnt >= totalCnt)
+			r = mid;
+		else
+			l = mid + 1;
+	}
+	return l;
+}
 
 void init() {
 	cin >> n;
-	server = vector<vector<int>>(n, vector<int>(n));
+	server = vector<int>(n * n);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> server[i][j];
-			totalCnt += server[i][j];
-		}
+	for (int i = 0; i < n * n; i++) {
+		cin >> server[i];
+		totalCnt += server[i];
+		maxFloor = max(maxFloor, server[i]);
 	}
 }
 
-int main(void) {
+int32_t main(void) {
 	fastio;
+	init();
 
+	cout << solve();
 }
