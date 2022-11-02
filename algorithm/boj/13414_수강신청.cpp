31f0f3496d55c724d653fa2d001a61303eb60db0
yoon <1004dustkd@gmail.com>
2022-11-02 23:44:13 +0900

    [algo]BOJ_13414

---
+++ algorithm/boj/13414_수강신청.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int k, l;
+
+void init() {
+	cin >> k >> l;
+}
+
+int main(void) {
+	fastio;
+
+}
