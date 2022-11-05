1f3cabd40aad9a8414cef738abdd57f1efde17e4
yoon <1004dustkd@gmail.com>
2022-11-05 23:28:45 +0900

    [algo]BOJ_16940

---
+++ algorithm/boj/16940_BFS 스페셜 저지.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int n;
+vector<int> Input;
+vector<vector<int>> adjlist;
+
+void init() {
+	cin >> n;
+	
+	adjlist = vector<vector<int>>(n + 1, vector<int>());
+	Input = vector<int>(n);
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		adjlist[a].push_back(b);
+		adjlist[b].push_back(a);
+	}
+	for (int i = 0; i < n; i++)
+		cin >> Input[i];
+}
+
+int main(void) {
+	fastio;
+	init();
+
+}
